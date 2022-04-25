20f1960392e803a21884784c8df65ffdea9b517f
amaiasanchis <amaiasanchisb@gmail.com>
2022-04-25 13:32:53 +0200

    iniciando programas 25/04

---
+++ primerprograma.py
@@ -0,0 +1,24 @@
+# comentarios
+# Este es un primer programa en python
+
+textunalinea = "este texto se asigna a una linea"
+
+textdistintaslineas = """Escribir valores alfanumericos 
+en distintas lineas"""
+
+#declaraciones de variables
+numero = 10
+Numero = 20
+saludo = "Hola mundo"
+
+print(numero)
+print(Numero)
+print(saludo)
+print(numero+Numero)
+print("saludos "+saludo)
+print(type(numero))
+print(type(saludo))
+del numero
+print(numero)
+
+
